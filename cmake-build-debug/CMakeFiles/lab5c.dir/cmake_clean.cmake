0fbf69fef0c32181daa948b230ea3e0987507b47
vladislavi_18 <ilcvlad922@gmail.com>
2022-03-12 16:37:02 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/lab5c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5c.dir/main.c.obj"
+  "lab5c.exe"
+  "lab5c.exe.manifest"
+  "lab5c.pdb"
+  "liblab5c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab5c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/lab5c.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lab5c.
+# This may be replaced when dependencies are built.
